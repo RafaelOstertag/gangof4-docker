c4ee89a9d68b26dbc73bcac0b7c4f80f986a104c
rafi@guengel.ch <rafi@guengel.ch>
2020-01-04 16:02:54 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -eu
+
+if [ $# -ne 1 ]
+then
+    echo "`basename $0` <VERSION>"
+    exit 1
+fi
+VERSION=$1
+
+pushd baseimage
+docker build -t baseimage:1.0.0 .
+popd
+
+pushd baseimage-build
+docker build -t baseimage-build:1.0.0 .
+popd
+
+pushd baseimage-runtime
+docker build -t baseimage-runtime:1.0.0 .
+popd
+
+pushd gangof4
+docker build -t gangof4:${VERSION} --build-arg VERSION=${VERSION} .
